ffc87d3230f278262bd28cba288d4ddf33c4771b
raedzayoud <raed.zayoud.23@gmail.com>
2025-09-19 17:56:21 +0200

    end of dashborad of seller

--- src/app/pages/tousventes/tousventes.ts
+++ src/app/pages/tousventes/tousventes.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VenteUserModel } from '../../services/models/venteuser';
+import { SalesService } from '../../services/api/sales/sales';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tousventes',
-  imports: [],
   templateUrl: './tousventes.html',
-  styleUrl: './tousventes.scss'
+  styleUrls: ['./tousventes.scss'],
+  imports: [NgFor, FormsModule],
 })
-export class Tousventes {
+export class Tousventes implements OnInit {
+  list: VenteUserModel[] = [];
+  idUser: number | null = null;
 
+  constructor(private saleService: SalesService) {}
+
+  ngOnInit(): void {
+    this.getVentePerSeller();
+  }
+
+  getVentePerSeller() {
+    this.idUser = Number(localStorage.getItem('id'));
+    if (this.idUser) {
+      this.saleService.getAllSalesBySeller(this.idUser).subscribe({
+        next: (response: any) => {
+          this.list = response.ventes;
+          console.log('suceessss===============');
+          console.log(this.list);
+        },
+        error: () => {
+          console.log('Error Fetching Data');
+        },
+      });
+    } else {
+      console.log('User ID not found in localStorage');
+    }
+  }
+
+  getUniqueClient(): string[] {
+    return [...new Set(this.list.map((v) => v.client_name))];
+  }
+
+  getVentesByClient(client: string): VenteUserModel[] {
+    return this.list.filter((v) => v.client_name === client);
+  }
 }
